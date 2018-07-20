21d4a36ec939fb3e362c0238adeab5f8d24dfd2a
Annika <annika.hansson@saffran.se>
2018-07-20 15:50:25 +0000

    Front page added.

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
+import calendar
 
+now = datetime.now()
+months = [" ","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Nov","Dec"]
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-        return "Hello World!"
+    thisMonth = months[now.month]
+    days = calendar.monthrange(now.year,now.month)
+    return render_template('index.html',i=1,year=now.year,month=thisMonth,days=days)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
