19877b47597b4ad740c6eba999b1d68a1151dd66
Blacka <annika.hansson@saffran.se>
2018-07-23 18:23:47 +0000

    Added like and dislike buttons for the posts.

--- palmtree/blog.py
+++ palmtree/blog.py
@@ -12,7 +12,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, likes, dislikes'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -34,8 +34,8 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
+                'INSERT INTO post (title, body, author_id, likes, dislikes)'
+                ' VALUES (?, ?, ?, 0, 0)',
                 (title, body, g.user['id'])
             )
             db.commit()
@@ -45,7 +45,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, likes, dislikes'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -85,6 +85,25 @@ def update(id):
             return redirect(url_for('blog.index'))
 
     return render_template('blog/update.html', post=post)
+
+@bp.route('/<int:id>/view')
+def view(id):
+	post = get_post(id,False)
+	return render_template('blog/view.html',post=post)
+
+@bp.route('/<int:id>/like', methods=('POST',))
+def status(id):
+	db = get_db()
+	if request.form['status'] == 'Like':
+		db.execute(
+			'UPDATE post SET likes = likes+1 WHERE id = ?', (id,))
+	elif request.form['status'] == 'Dislike':
+		db.execute(
+			'UPDATE post SET dislikes = dislikes+1 WHERE id = ?', (id,))
+	else:
+		flash('Unknown status.')
+	db.commit()
+	return redirect(url_for('blog.view', id=id))
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
